7ada1921f7e714b7786c17b261638264f8155321
Gregor Ziegltrum <gregor@trum.eu>
2022-01-17 15:21:30 +0100

    Brits converter script

---
+++ preProcessor/converttobrits.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 17 11:14:04 2022
+
+@author: gregor
+
+Uses the exported data an convert it in a format for putting it into BRITS
+
+
+
+"""
+"""
+cat_count = {}
+for x in data:
+    for y in x:
+        l = len(set(y['category']))
+        if l not in cat_count:
+            cat_count[l] = 0
+        cat_count[l] += 1
+"""          
+import pickle
+from classifier import categories
+import numpy as np
+
+len_x_t = 16
+
+def parse_delta(masks, backward=False):
+    if backward:
+        masks = masks[::-1]
+
+    deltas = []
+
+    for h in range(len(masks)):
+        if h == 0:
+            deltas.append(np.ones(len_x_t))
+        else:
+            deltas.append(np.ones(len_x_t) + (1 - masks[h]) * deltas[-1])
+    return np.array(deltas)
+
+
+def convert_meal_to_brits(curr_meal):
+    brits_day_data = []
+    max_cat = 7 #if we only use 7 categories we have 95% of data included
+    meal = 0
+    if curr_meal['meal'] == 'breakfast':
+        meal = 1
+    elif curr_meal['meal'] == 'lunch':
+        meal = 2
+    elif curr_meal['meal'] == 'snacks':
+        meal = 4
+    elif curr_meal['meal'] == 'dinner':
+        meal = 3
+    else:
+        raise Exception(f"Meal not detected {curr_meal[meal]}")
+    
+    #convert to category numbers
+    daily_categories = [-1 for _ in range(max_cat)]
+    for i,c in enumerate(curr_meal['category']):
+        if i == max_cat:
+            break
+        daily_categories[i] = categories.index(c)
+    daily_categories.sort(reverse=True)
+    
+    brits_day_data.append(meal)
+    brits_day_data.append(curr_meal['calories'])
+    brits_day_data.append(curr_meal['carbs'])
+    brits_day_data.append(curr_meal['fat'])
+    brits_day_data.append(curr_meal['protein'])
+    brits_day_data.append(curr_meal['cholest'])
+    brits_day_data.append(curr_meal['sodium'])
+    brits_day_data.append(curr_meal['sugar'])
+    brits_day_data.append(curr_meal['fiber'])
+    
+    brits_day_data.extend(daily_categories)
+    if len(brits_day_data) != len_x_t:
+        raise Exception("len x_t divertes from what it should be!")
+    return brits_day_data
+
+def convert_series_to_brits(series):
+    return np.array([convert_meal_to_brits(s) for s in series])
+    
+   
+def convert_time_series(values,masks,deltas,evals,eval_masks):
+    time_steps = []
+    for i in range(len(evals)):
+        entry = {
+            'values': values[i].tolist(),
+            'masks': masks[i].tolist(),
+            'deltas':deltas[i].tolist(),
+            'evals':evals[i].tolist(),
+            'eval_masks':eval_masks[i].tolist()
+            }
+        time_steps.append(entry)    
+    return time_steps
+     
+        
+file = open('time_data.pickle', 'rb')
+data = pickle.load(file)
+file.close()
+
+
+series = data[0]
+evals = convert_series_to_brits(series)
+drop_meal_indices = [9,10]#np.random.choice(range(len(evals)),len(evals)//10)
+masks = np.ones((len(evals),len_x_t))
+eval_masks = np.zeros((len(evals),len_x_t))
+values = evals.copy()
+for index in drop_meal_indices:
+    values[index] = np.zeros(len_x_t)
+    masks[index] = np.zeros(len_x_t)
+    eval_masks[index] = np.ones(len_x_t)
+
+deltas = parse_delta(masks)
+deltas_back = parse_delta(masks,backward =True)
+
+forwards = convert_time_series(values,masks,deltas,evals,eval_masks)
+backwards = convert_time_series(values[::-1],masks[::-1],deltas_back,evals[::-1],eval_masks[::-1])
--- preProcessor/export.py
+++ preProcessor/export.py
@@ -9,6 +9,8 @@ import sqlite3
 import datetime
 from classifier import FoodClassificationCnnModel,Classifier
 import time
+import json
+import pickle
 
 # Gets all meals for a user for days where there are mor than 1600 kcal and at least breakfast lunch and dinner
 select_meal_history_filtered_by_user = """
@@ -16,9 +18,9 @@ select * from meal_history_flat mlf
 where mlf.meal in ('breakfast','lunch','dinner','snacks') and
 mlf.name not like 'Quick Add%' and
 mlf.user = ? and
-    exists (select * from meal_history_flat mlf3 where mlf3.date = mlf.date and mlf3.user = mlf.user and mlf3.meal = 'breakfast') and
-    exists (select * from meal_history_flat mlf3 where mlf3.date = mlf.date and mlf3.user = mlf.user and mlf3.meal = 'lunch') and
-    exists (select * from meal_history_flat mlf3 where mlf3.date = mlf.date and mlf3.user = mlf.user and mlf3.meal = 'dinner') and
+    exists (select * from meal_history_flat mlf3 where mlf3.date = mlf.date and mlf3.user = mlf.user and mlf3.meal = 'breakfast' and mlf3.name not like 'Quick Add%' ) and
+    exists (select * from meal_history_flat mlf3 where mlf3.date = mlf.date and mlf3.user = mlf.user and mlf3.meal = 'lunch' and mlf3.name not like 'Quick Add%') and
+    exists (select * from meal_history_flat mlf3 where mlf3.date = mlf.date and mlf3.user = mlf.user and mlf3.meal = 'dinner' and mlf3.name not like 'Quick Add%') and
     exists (select * from (select sum(calories) s from meal_history_flat mlf2
                        where mlf2.user = mlf.user and mlf2.date = mlf.date and
                        mlf2.meal in ('breakfast','lunch','dinner','snacks')) a
@@ -38,9 +40,9 @@ exists (select * from (select sum(calories) s from meal_history_flat mlf2
 """
 
 select_user_with_history = """
-select user from user 
-where food_crawl_time is not null and 
-exists(select * from meal_history_flat mlf where mlf.user = user)
+select u.user from user u
+where u.food_crawl_time is not null and
+exists(select * from meal_history_flat mlf where mlf.user = u.user) limit 3000
 """
 
 def get_meal_history_flat_filtered_by_user_id(con, user, snacks=True):
@@ -51,7 +53,7 @@ def get_meal_history_flat_filtered_by_user_id(con, user, snacks=True):
       cur.execute(select_meal_history_filtered_by_user_no_snacks , (user,))
   res = cur.fetchall()
   res = [{#'id': x[0],
-          'date': datetime.datetime.strptime(x[1],'%Y-%m-%d').date(),
+          'date': datetime.datetime.strptime(x[1],'%d-%m-%y').date(),
           #'orig-date': x[1],
           'meal': x[2],
           'user': x[3],
@@ -74,9 +76,13 @@ def get_user_ids_with_history(con):
     cur.execute(select_user_with_history)
     res = cur.fetchall()
     res = [x[0] for x in res]
+    cur.close()
     return res
 
 def extract_fragments_from_meals(meal_list):
+    """Extract fragments from one users data where there are at least 7 days in a row and put
+    them in fragments of 7 days"""
+    fragment_size = 7 
     meal_list = meal_list.copy()
     meal_list.sort(key= lambda x: x['date'])
     datewise = {}
@@ -89,7 +95,6 @@ def extract_fragments_from_meals(meal_list):
     
     user_fragments = []
     fragment_list = []
-    fragment_size = 7 
     last_date = datetime.date.min
     for curr_date,food_items in datewise.items():
         delta = curr_date-last_date
@@ -98,34 +103,35 @@ def extract_fragments_from_meals(meal_list):
         if delta.days > 1:
             fragment_list = []
         last_date = curr_date
-        fragment_list.append((curr_date,food_items))
+        fragment_list.append(food_items)
         if len(fragment_list) == fragment_size:
             user_fragments.append(fragment_list)
             fragment_list = []
             last_date = datetime.date.min
     return user_fragments
 
-def classify_fragments(fragments):
-    classy = Classifier("../preProcessor/data/models")
-    
+def classify_fragments(classy,fragments):
+    """classifies the extracted fragments into food categories using a pretrained categoriser"""
     for x in fragments:  
-        for date,meal_items in x:
+        for meal_items in x:
             for meal_item in meal_items:
                 if meal_item['name'] is None:
-                    meal_item['category'] = -1
+                    meal_item['category'] = None
                 else:
-                    meal_item['category'] = int(classy.classify(meal_item['name']))
+                    meal_item['category'] = classy.get_cat_name(classy.classify(meal_item['name']))
     
 
-
 def fill_up_empty_snacks(fragments):
+    """creates an empty entry for snacks if no snacks are there. 
+    All other meal types must exists"""
     for x in fragments:  
-        for date,meal_items in x:
+        for meal_items in x:
             if next((x for x in meal_items if x['meal'] == 'snacks'),None) is None:
                 meal_items.append({'date': meal_items[0]['date'],
                                    'meal':'snacks',
                                    'user': meal_items[0]['user'],
                                    'name': None,
+                                   'category': None,
                                    'calories': 0,
                                    'carbs': 0,
                                    'fat': 0,
@@ -135,16 +141,61 @@ def fill_up_empty_snacks(fragments):
                                    'sugar': 0,
                                    'fiber':0})
         
+def process_fragments(classy,frags):
+    """process the fragments and changes the frags variable"""
+    classify_fragments(classy,frags)
+    fill_up_empty_snacks(frags)
 
-#t0 = time.time()
-#classy.classify("Chilli Cheese Burger")
-#print(time.time()-t0)
+def combine(b):
+    """combine all food items of one meal type by adding up the nutrients 
+    and make a list of categories. We ignore everything that is None ergo 0 Imputation"""
+    z = {'date': b[0]['date'],
+         'meal':b[0]['meal'],
+         'user': b[0]['user'],
+         'category': [x['category'] for x in b if x['category'] is not None],
+         'calories': sum([x['calories'] for x in b]),
+         'carbs': sum([x['carbs'] for x in b if x['carbs'] is not None]),
+         'fat': sum([x['fat'] for x in b if x['fat'] is not None]),
+         'protein': sum([x['protein'] for x in b if x['protein'] is not None]),
+         'cholest': sum([x['cholest'] for x in b if x['cholest'] is not None]),
+         'sodium':sum([x['sodium'] for x in b if x['sodium'] is not None]),
+         'sugar': sum([x['sugar'] for x in b if x['sugar'] is not None]),
+         'fiber':sum([x['fiber'] for x in b if x['fiber'] is not None])}
+    return z
+
+def convert_to_time_series(frags):
+    """Convert fragments into a time series data"""
+    time_fragments = []
+    for fragment in frags:
+        user_fragment = []
+        for day in fragment:
+            user_fragment.append(combine([x for x in day if x['meal'] == 'breakfast']))
+            user_fragment.append(combine([x for x in day if x['meal'] == 'lunch']))
+            user_fragment.append(combine([x for x in day if x['meal'] == 'dinner']))
+            user_fragment.append(combine([x for x in day if x['meal'] == 'snacks']))
+        time_fragments.append(user_fragment)
+    return time_fragments
+
+t0 = time.time()
+classy = Classifier("../preProcessor/data/models")
 con = sqlite3.connect("../preProcessor/data/mfp.db")
 user_ids = get_user_ids_with_history(con)
-l = get_meal_history_flat_filtered_by_user_id(con, 9225,snacks=True)
-frags = extract_fragments_from_meals(l)
-classify_fragments(frags)
-fill_up_empty_snacks(frags)
+time_series = []
+for x in user_ids:
+    print(f"Curr len: {len(time_series)} Now Processing: {x}")
+    l = get_meal_history_flat_filtered_by_user_id(con, x,snacks=True)
+    frags = extract_fragments_from_meals(l)
+    process_fragments(classy,frags)
+    time_series.extend(convert_to_time_series(frags))
+    if len(time_series) > 400:
+        break
+t1 = time.time()
+print(f"TIme Elapsed {(t1-t0):2f}")
+print(len(time_series))
 
+with open('time_data.pickle', 'wb') as outfile:
+    pickle.dump(time_series, outfile)
 
+#with open('data.json', 'w') as f:
+#    json.dump(data, f)
 
